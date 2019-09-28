23c96931b984a20b16fe7a0f75218ef5aaddd6be
Lengeli <lengel@spse-po.sk>
2019-09-28 21:02:01 +0200

    pridanie vysuvacieho menu

--- app/src/main/java/com/example/projectsoc/MainActivity.java
+++ app/src/main/java/com/example/projectsoc/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.projectsoc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
