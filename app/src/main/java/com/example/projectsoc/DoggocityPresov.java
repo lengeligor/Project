e0c613754cde813354034c418b14ea7b71c95397
Lengeli <lengel@spse-po.sk>
2019-09-29 13:05:38 +0200

    pridanie uvodneho zapinania apky

---
+++ app/src/main/java/com/example/projectsoc/DoggocityPresov.java
@@ -0,0 +1,53 @@
+package com.example.projectsoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DoggocityPresov extends AppCompatActivity implements View.OnClickListener{
+    private RelativeLayout layout;
+    private Button enter;
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doggocity_presov);
+        initialize();
+        enter.setOnClickListener(this);
+    }
+
+    private void initialize(){
+        layout = findViewById(R.id.relativelayout_activityMain);
+        appName = findViewById(R.id.first_text_appName);
+        enter = findViewById(R.id.enter_button);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        layout.startAnimation(animation);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == enter){
+            startActivity(new Intent(DoggocityPresov.this,MainActivity.class));
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+        }
+    }
+
+    @Override
+    public void finish(){
+        super.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
