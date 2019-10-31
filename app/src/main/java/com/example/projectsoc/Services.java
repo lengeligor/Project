5b245e289d6fd2c5872b80034df12db4c6ebacf0
Lengeli <lengel@spse-po.sk>
2019-10-31 10:03:01 +0100

    pridanie fragmentu mapy a zobrazenie tvojej polohy

--- app/src/main/java/com/example/projectsoc/Services.java
+++ app/src/main/java/com/example/projectsoc/Services.java
@@ -1,23 +1,99 @@
 package com.example.projectsoc;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-public class Services extends Fragment {
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class Services extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mGoogleMap;
+    private View mView;
+    private FusedLocationProviderClient client;
+
+    private String latitude;
+    private String longtitude;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layoutInflater = inflater.inflate(R.layout.services_fragment,container,false);
+        mView = inflater.inflate(R.layout.services_fragment,container,false);
+        requestPermission();
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
 
 
+        return mView;
+    }
 
-        return layoutInflater;
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapView mMapView = (MapView) mView.findViewById(R.id.map);
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+
+        client.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if( location != null ){
+                    latitude = String.valueOf(location.getLatitude());
+                    longtitude = String.valueOf(location.getLongitude());
+                    System.out.println(latitude);
+                    System.out.println(longtitude);
+                    LatLng positionBasic = new LatLng(Double.parseDouble(latitude),Double.parseDouble(longtitude));
+                    mGoogleMap.addMarker(new MarkerOptions().position(positionBasic).title("Here"));
+                    Toast.makeText(getContext(),"Tu sa nachádzaš",Toast.LENGTH_SHORT).show();
+                    CameraPosition cameraPositionBasic = CameraPosition.builder().target(positionBasic).zoom(16).bearing(0)
+                            .tilt(45).build();
+                    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPositionBasic));
+
+                }
+            }
+        });
+
+
+    }
+
+    public void requestPermission(){
+        ActivityCompat.requestPermissions(getActivity(),new String[]{ACCESS_FINE_LOCATION},1);
     }
 }
